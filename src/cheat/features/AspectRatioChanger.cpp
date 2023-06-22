914f1e74c3e9199e71fd09c62a592ba3e3862186
oxiKKK <danekgavlas1@email.cz>
2023-06-22 16:33:02 +0200

    [cheat] Added AspectRatioChanger source files.

---
+++ src/cheat/features/AspectRatioChanger.cpp
@@ -0,0 +1,42 @@
+/*
+*	OXWARE developed by oxiKKK
+*	Copyright (c) 2023
+* 
+*	This program is licensed under the MIT license. By downloading, copying, 
+*	installing or using this software you agree to this license.
+*
+*	License Agreement
+*
+*	Permission is hereby granted, free of charge, to any person obtaining a 
+*	copy of this software and associated documentation files (the "Software"), 
+*	to deal in the Software without restriction, including without limitation 
+*	the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+*	and/or sell copies of the Software, and to permit persons to whom the 
+*	Software is furnished to do so, subject to the following conditions:
+*
+*	The above copyright notice and this permission notice shall be included 
+*	in all copies or substantial portions of the Software. 
+*
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+*	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+*	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+*	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+*	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+*	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+*	IN THE SOFTWARE.
+*/
+
+#include "precompiled.h"
+
+VarBoolean aspect_ratio_enable("aspect_ratio_enable", "Enables custom aspect ratio", false);
+VarFloat aspect_ratio_value("aspect_ratio_value", "Aspect ratio value", 3.0f / 4.0f, 0.5f, 2.0f);
+
+double CAspectRatioChanger::scale_aspect()
+{
+	if (!aspect_ratio_enable.get_value())
+	{
+		return -1.0;
+	}
+
+	return aspect_ratio_value.get_value();
+}
